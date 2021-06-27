158dcdea756c0f9fc416bf306b09e2bdda49599b
auki <39486149+Kesigner@users.noreply.github.com>
2021-06-27 20:27:14 +0800

    Update and rename miaona-soga.sh to unicorn-soga.sh

--- unicorn-config/unicorn-soga.sh
+++ unicorn-config/unicorn-soga.sh
@@ -5,7 +5,7 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-download_miaona(){
+download_unicorn(){
 	echo "正在安装soga . . ."
 	bash <(curl -Ls https://blog.sprov.xyz/soga.sh)
 	echo "正在同步时间 . . ."
@@ -16,8 +16,8 @@ download_miaona(){
 	echo "正在更新配置文件 . . ."
 	rm -f /etc/soga/soga.conf
 	rm -f /etc/soga/blockList
-	wget -P /etc/soga https://raw.githubusercontent.com/mps1209/miaoconfig/master/soga.conf
-	wget -P /etc/soga https://raw.githubusercontent.com/mps1209/miaoconfig/master/blockList
+	wget -P /etc/soga https://raw.githubusercontent.com/Kesigner/unicorn/main/unicorn-config/unicorn-soga.sh
+	wget -P /etc/soga https://raw.githubusercontent.com/Kesigner/unicorn/main/unicorn-config/blockList
 	cd /etc/soga
 	printf "请输入节点ID："
 	read -r nodeId <&1
@@ -26,28 +26,16 @@ download_miaona(){
 	shon_online
 }
 
-start_miaona(){
+start_unicorn(){
 	echo "正在启动soga . . ."
 	soga start
-	shon_online
-}
-
-uninstall_posidon(){
-	echo "正在卸载posidon . . ."
-	set -x
-	systemctl stop v2ray
-	systemctl disable v2ray
-	service v2ray stop
-	update-rc.d -f v2ray remove
-	rm -rf /usr/bin/v2ray /etc/init.d/v2ray /lib/systemd/system/v2ray.service
-	set -
 	shon_online
 }
 
 add_shenji(){
 	echo "正在添加审计 . . ."
     	rm -f /etc/soga/blockList
-    	wget -P /etc/soga https://raw.githubusercontent.com/mps1209/miaoconfig/master/blockList
+    	wget -P /etc/soga https://raw.githubusercontent.com/Kesigner/unicorn/main/unicorn-config/blockList
     	soga restart
 	shon_online
 }
@@ -57,9 +45,8 @@ echo "请选择您需要进行的操作:"
 echo "1) 安装 soga"
 echo "2) 启动 soga"
 echo "3) 查看 soga状态"
-echo "4) 卸载 posidon"
-echo "5) 添加审计"
-echo "6) 退出脚本"
+echo "4) 添加审计"
+echo "5) 退出脚本"
 echo "   Version：1.0.0"
 echo ""
 echo -n "   请输入编号: "
@@ -68,9 +55,8 @@ case $N in
   1) download_miaona ;;
   2) start_miaona ;;
   3) soga status ;;
-  4) uninstall_posidon ;;
-  5) add_shenji ;;
-  6) exit ;;
+  4) add_shenji ;;
+  5) exit ;;
   *) echo "Wrong input!" ;;
 esac 
 }
